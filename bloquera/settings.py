bc8e818acbab08eab469deaba41d26f7d73becb4
PABM25 <pilar.andrea1996@gmail.com>
2025-11-10 14:45:49 -0300

    Arreglo de todos los problemas

--- bloquera/settings.py
+++ bloquera/settings.py
@@ -107,7 +107,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_collected'
 STATICFILES_DIRS = [
      BASE_DIR / "core" / "static",
 ]
